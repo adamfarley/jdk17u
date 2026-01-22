eab51bd37919639095b110d0bb103edce935aeb0
Adam Farley <adfarley@ibm.com>
2026-01-22 12:52:35 +0000

    Update Win8365790Test.java

--- test/jdk/tools/jpackage/windows/Win8365790Test.java
+++ test/jdk/tools/jpackage/windows/Win8365790Test.java
@@ -42,6 +42,7 @@ import jdk.jpackage.test.TKit;
  * @test
  * @summary Test case for JDK-8365790
  * @library /test/jdk/tools/jpackage/helpers
+ * @library base
  * @build jtreg.SkippedException
  * @build jdk.jpackage.test.*
  * @build Win8365790Test
